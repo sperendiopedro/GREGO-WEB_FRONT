fc9430575ae03b47a52d8eecc6f2954ad9d3f38c
pedro <pedro.sperendio@outlook.com>
2024-07-26 15:58:52 -0300

    alterações finais 26-07

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,16 +11,15 @@ function Header(){
             <Link className="cadastros" to="/"> Mercadoria </Link>
             <Link className="cadastros" to="/"> Moeda </Link>
             <div className="dropdown">
-                <Link className="dropdown-toggle" to="/">Cadastros gerais</Link>
-                <div className="dropdown-menu">
-                    <Link className="dropdown-item" to="/user/register">Registro de usuário</Link>
-                    <Link className="dropdown-item" to="/ufd/register">Registro de UF</Link>
-                    <Link className="dropdown-item" to="/empresa/register">Registro de empresa</Link>
-                    <Link className="dropdown-item" to="/cadastros/item4">Registro de fornecedor</Link>
-                </div>
+            <div className="dropdown-toggle">Cadastros gerais</div>
+            <div className="dropdown-menu">
+                <Link className="dropdown-item" to="/user/register">Registro de usuário</Link>
+                <Link className="dropdown-item" to="/ufd/register">Registro de UF</Link>
+                <Link className="dropdown-item" to="/empresa/register">Registro de empresa</Link>
+                <Link className="dropdown-item" to="/cadastros/item4">Registro de fornecedor</Link>
             </div>
+        </div>
             
-            <h1> </h1>
 
         </header>
     )
